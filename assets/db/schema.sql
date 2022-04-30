9a61702b6501ad1a48318a5ce0e44d5fb6403a40
iggy <allyson.nostrandd@gmail.com>
2022-04-30 11:19:53 -0700

    created department table

---
+++ assets/db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+   id INT NOT NULL, 
+   department_name VARCHAR(30),
+   PRIMARY KEY (id)
+);
+
