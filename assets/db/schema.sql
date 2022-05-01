0da9c14268df0a15ddf5865b88649606a2d4241d
iggy <allyson.nostrandd@gmail.com>
2022-05-01 09:45:42 -0700

    schema.sql tables sucessfully built

--- assets/db/schema.sql
+++ assets/db/schema.sql
@@ -5,14 +5,27 @@ USE employees_db;
 
 CREATE TABLE department (
    id INT NOT NULL, 
-   department_name VARCHAR(30),
+   department_name VARCHAR(30) NOT NULL,
    PRIMARY KEY (id)
 );
 
 CREATE TABLE roles (
-    id INT,
-    title VARCHAR(30),
-    salary DECIMAL,
-    FOREIGN KEY (department_id),
-    REFERENCES department(id)
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL, FOREIGN KEY (department_id) REFERENCES department(id)
 );
+
+CREATE TABLE employee (
+    id INT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL, FOREIGN KEY (role_id) REFERENCES roles(id),
+    manager_id INT, FOREIGN KEY (manager_id) REFERENCES roles(id)
+);
+
+DESCRIBE department;
+
+DESCRIBE roles;
+
+DESCRIBE employee;
