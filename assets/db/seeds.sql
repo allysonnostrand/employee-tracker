2e1374a969f9e866dbe78a0e9a8eaa2bccfc640a
iggy <allyson.nostrandd@gmail.com>
2022-05-01 17:38:23 -0700

    downloaded npm packages and can connect to db through index.js file

--- assets/db/seeds.sql
+++ assets/db/seeds.sql
@@ -9,7 +9,7 @@ VALUES  (10,"tester", 20.0, 1),
         (30, "some more stuff do-er", 10.1, 3);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES  (15,"bob", "builder", 20),
+VALUES  (15,"bob", "builder", 20, NULL),
         (15,"jill", "builds", 10, 20),
         (15,"tom", "lazy", 30, 20);
 
