636795843bfd7b5d8ac1e49fec83e5e9b70bfc5b
iggy <allyson.nostrandd@gmail.com>
2022-05-01 17:18:41 -0700

    added tester seeds to seeds.sql

---
+++ assets/db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO department (id, department_name)
+VALUES  (1,"testing"),
+        (2, "stuff"),
+        (3, "some more stuff");
+
+INSERT INTO roles (id, title, salary, department_id)
+VALUES  (10,"tester", 20.0, 1),
+        (20, "stuff do-er", 22.1, 2),
+        (30, "some more stuff do-er", 10.1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES  (15,"bob", "builder", 20),
+        (15,"jill", "builds", 10, 20),
+        (15,"tom", "lazy", 30, 20);
+
+
+SELECT * FROM department;
+
+SELECT * FROM roles;
+
+SELECT * FROM employee;
