8d72bf05fc25c05a4aa5205112caea8b27df6f16
iggy <allyson.nostrandd@gmail.com>
2022-05-02 22:35:04 -0700

    updated view all employees to show mega joined table

--- index.js
+++ index.js
@@ -24,7 +24,32 @@ const db = mysql.createConnection(
           ]).then(answers =>{
                 switch (answers.menuOpts){
                     case 'view all employees':
-                        db.query('SELECT * FROM employee', (err,res) => {
+                        db.query(`SELECT 
+                            erd.id,
+                            erd.first_name,
+                            erd.last_name,
+                            erd.role_id,
+                            erd.title,
+                            erd.salary,
+                            erd.department_id,
+                            erd.department_name,
+                            m.first_name manager_first,
+                            m.last_name manager_last
+                            FROM (SELECT * FROM employee e LEFT JOIN 
+                                (
+                                    SELECT 
+                                        r.id role_id,
+                                        title,
+                                        salary,
+                                        department_id,
+                                        department_name
+                                    FROM roles r 
+                                    LEFT JOIN department d
+                                    ON r.department_id = d.id
+                                ) rd
+                            USING (role_id)) erd
+                            LEFT JOIN employee m
+                            ON erd.manager_id = m.id`, (err,res) => {
                             console.table(res);
                             menu();
                         })
@@ -163,5 +188,4 @@ function addDepartment(){
         })
 }
 
-
-  menu();
+menu();
